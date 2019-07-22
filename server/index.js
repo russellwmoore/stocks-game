4c6b79b847531c6379bbff98ceb7a3735e74bdd9
Russell Moore <russellwmoore@gmail.com>
2019-07-22 15:28:46 -0400

    Cookie Parse middleware add

--- server/index.js
+++ server/index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const morgan = require('morgan');
 const chalk = require('chalk');
 const path = require('path');
+const cookieparser = require('cookie-parser');
+const SID = 'SID';
+const uuidv4 = require('uuid/v4');
 
 const app = express();
 const PORT = 3000;
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cookieparser());
+
+app.use((req, res, next) => {
+  if (!req.cookies[SID]) {
+    res.cookie(SID, uuidv4());
+  }
+  next();
+});
 
 app.use('/api', require('./api'));
 
