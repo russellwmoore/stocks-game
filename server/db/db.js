854c8389090be84b0914e42a6f9bc88aa087100f
Russell Moore <russellwmoore@gmail.com>
2019-08-26 12:50:55 -0400

    Change db connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,7 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/ttp', { logging: false });
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/ttp',
+  { logging: false }
+);
 
 module.exports = db;
